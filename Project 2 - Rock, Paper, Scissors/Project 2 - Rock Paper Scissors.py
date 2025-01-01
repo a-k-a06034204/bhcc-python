a7f9cda71bfea0b88409e7f41ca3442f6eb1a0ec
terra-ko <teakochapski@outlook.com>
2025-01-01 18:20:22 -0500

    Add files via upload

--- Project 2 - Rock, Paper, Scissors/Project 2 - Rock Paper Scissors.py
+++ Project 2 - Rock, Paper, Scissors/Project 2 - Rock Paper Scissors.py
@@ -1,38 +1,65 @@
-#By terra-ko
-#Project 2 Rock Paper Scissors
+# Project 2 - Rock, Paper, Scissors
+# By: Ashley Kochapski
 
 import random
 
-wepons = ("R,r,P,p,S,s")
-running = True
-#count wins, losses, ties, and how many each user selected each weapon
+# Count wins, losses, ties, and how many each user selected each weapon
 
+weapons = ["R","r","P","p","S","s"]
+player_chose_rock = 0
+player_chose_paper = 0
+player_chose_scissors = 0
+player_wins = 0
+cpu_wins = 0
+ties = 0
+
+print("Hello user! :)")
+running = True
 while running: 
-    player = None
+    player = ''
     cpu = random.choice(weapons)
 
-while player not in weapons:
-    player = input("Rock, Paper, Scissors? ")
-    print(f"Player:{player}")
-    print(f"cpu:{cpu}")
+    # Make the player choose a weapon:
+    while player not in weapons:
+        player = input("Rock (R), Paper (P), Scissors (S)? ")
+    print(f"Player: {player}")
+    print(f"CPU: {cpu}")
 
-if player == cpu:
-    print("We've tied!")
+    # Count the weapon choices
+    if player.lower() == 'r':
+        player_chose_rock += 1
+    if player.lower() == 'p':
+        player_chose_paper += 1
+    if player.lower() == 's':
+        player_chose_scissors += 1
 
-elif player == "Rock" and cpu == "Scissors":
-    print("Looks like you won this time!")
+    # Count the wins:
+    if player == cpu:
+        print("We've tied!")
+        ties += 1
+    elif player == "r" and cpu == "s":
+        print("Looks like you won this time!")
+        player_wins += 1
+    elif player == "p" and cpu == "r":
+        print("Looks like you won this time!")
+        player_wins += 1
+    elif player == "s" and cpu == "p":
+        print("Looks like you won this time!")
+        player_wins += 1
+    else:
+        print("Sorry, you lose!")
+        cpu_wins += 1
 
-elif player == "Paper" and cpu == "Rock":
-    print("Looks like you won this time!")
+    # Ask if they want to play another round:
+    if not input("Would you like to play again? (y/n): ").lower() == "y":
+        running = False
 
-elif player == "Scissors" and cpu == "Paper":
-    print("Looks like you won this time!")
-
-else:
-    print("Sorry, you lose!")
-
-if not input("Would you like to play again? (y/n): ").lower() == "y":
-    running = False
-    #Qq to quit/"are you sure you want to quit?"
+# Show the results:
+print(f"CPU wins: {cpu_wins}")
+print(f"Player wins: {player_wins}")
+print(f"Rounds that ended in a tie: {ties}")
+print(f"The number of times the user selected each weapon (Rock, Paper, Scissors):")
+print(f"- Rock: {player_chose_rock}")
+print(f"- Paper: {player_chose_paper}")
+print(f"- Scissors: {player_chose_scissors}")
 print("Thank you for playing, goodbye!") 
-
